5098e7a434ba68ea6115eb79451ee542367169b3
TsukadaTomoya <xftpt.nonoka@gmail.com>
2019-08-11 10:41:37 +0900

    little adjustment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,49 @@
 class PostsController < ApplicationController
 
   def new
-    
-  end
-  
-  def show
-  
-  end
-
-  def edit
-
-  end
-
-  def index
-
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to user_url
+    else
+      render 'posts/new'
+    end
+  end
+  
+  def show
+    @post = Post.where(user_id: current_user.id)
+  end
 
+  def edit
+    
+  end
+
+  def update
+    if @post = current_user.posts.update_attributes(post_params)
+      flash[:notice] = "編集が完了しました"
+      redirect_to user_url
+    else
+      render 'posts/edit'
+    end
+  end
+
+  def index
+    @post = Post.all
   end
 
   def destroy
-
+    @post.destroy
+    flash[:notice] = "投稿が削除されました"
+    redirect_to user_url
   end
+
+    private
+
+      def post_params
+        params.require(:post).permit(:content, :language)
+      end
 end
