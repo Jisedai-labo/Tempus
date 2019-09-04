08df1ee9f4f39a53b30e3883397814e76a1563ad
Shunta Watabe <s.ubel.w.w.09@gmail.com>
2019-09-04 13:21:52 +0900

    create favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_061951) do
+ActiveRecord::Schema.define(version: 2019_09_01_083511) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
