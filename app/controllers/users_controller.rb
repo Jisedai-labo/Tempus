f0b42f7b96bd76f214fd91fc647089a7db1f1ff2
TsukadaTomoya <xftpt.nonoka@gmail.com>
2019-09-05 17:32:30 +0900

    comments to solve question

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
-    language = Language.all.map(&:language)
+    language = Language.all.map(&:language) #言語一覧
+    #11,7の空の2次元配列を作る(これをハッシュにネストして、言語ごとに毎日どれだけ勉強したかをハッシュにしたい)
     list_of_studytime = Array.new(language.size, Array.new(7, 0))
+    #キーを言語、値を空の配列にしたハッシュを作成
+    #stats = {"HTML/CSS" => [0,0,.. 0,100], ..}みたいな感じです
     @stats = language.zip(list_of_studytime).to_h
 
     7.times do |i|
+      #1日の言語ごとの勉強時間をハッシュにする=>i日前の勉強時間を取得して、statsのキーが一致した所に勉強時間を入れたい
       temp = @posts.where(created_at: Time.current.ago(i.days).all_day).pluck(:language, :studytime).to_h
       
       temp.each do |key, value|
