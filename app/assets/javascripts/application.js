82a74c58697a2e4202cc9077fd7f749c0d52449d
TsukadaTomoya <xftpt.nonoka@gmail.com>
2019-09-09 01:07:43 +0900

    add 'return;' to stats.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require Chart.min
 //= require rails-ujs
 //= require jquery
 //= require activestorage
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require turbolinks
+//= require Chart.min
 //= require_tree .
