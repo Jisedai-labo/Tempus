286ae1c17efdf7af8e7170888bc8cc4334bf8a55
TsukadaTomoya <xftpt.nonoka@gmail.com>
2019-09-08 21:54:40 +0900

    add comments for users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,21 +11,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts
     list_of_languages = Language.all.pluck(:language) #言語一覧
-    #11,7の空の2次元配列を作る(これをハッシュにネストして、言語ごとに毎日どれだけ勉強したかをハッシュにしたい)
+    #大きさ11の配列=>これに言語ごとの勉強時間の配列(1週間分)を入れていく
     list_of_studytime = Array.new(list_of_languages.size)
-    #キーを言語、値を空の配列にしたハッシュを作成
-    #stats = {"HTML/CSS" => [0,0,.. 0,100], ..}みたいな感じです
-    studytime = Array.new(7)
 
+    #言語の種類の数だけループを回し、各言語ごとに勉強時間の配列を作る
     11.times do |i|
+      #list_of_studytimeに入れる.初期値は[0,0,0,0,0,0,0]
+      #例えばi=1なら、list_of_languages[1]に入ってる言語の勉強時間を入れる
+      #今回は言語と勉強時間分けてるけれど、デバッグする時考えると言語をキーにしたハッシュにすべきでした
       temp_studytime = Array.new(7, 0)
+
       7.times do |j|
-        #1日の言語ごとの勉強時間をハッシュにする=>i日前の勉強時間を取得して、statsのキーが一致した所に勉強時間を入れたい
+        #1日分の時間を変数にとる
         timerange = Time.current.ago(j.days).beginning_of_day..Time.current.ago(j.days).end_of_day
+        #tempに言語・timerangeで絞り込んで得られた勉強時間を格納
         temp = @posts.where(created_at: timerange, language: list_of_languages[i]).pluck(:studytime)
 
         temp_studytime[j] = temp.sum
       end
+      #ループの最後にその言語の1週間の勉強時間をlist_of_studytimeに入れる
       list_of_studytime[i] = temp_studytime
     end
 
